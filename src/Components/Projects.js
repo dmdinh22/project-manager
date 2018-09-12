673245d9b79da85cb4a59781911202d35a2d9787
David Dinh <dmdinh22@gmail.com>
2018-09-12 09:11:53 -0700

    initial commit

---
+++ src/Components/Projects.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    render() {
+        return <div className="Projects">My Projects {this.props.test}</div>;
+    }
+}
+
+export default Projects;
